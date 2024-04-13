1a0a4882419ad0f26041cde8617644c15d6bfdea
shanthan katkuri <shanthanreddykatkuri@gmail.com>
2024-04-13 16:45:56 +0530

    v1

---
+++ components/Navbar.tsx
@@ -0,0 +1,33 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import MobileNav from './MobileNav'
+import { UserButton } from '@clerk/nextjs'
+
+const Navbar = () => {
+  return (
+    <nav className='flex flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10'>
+      <Link href="/" className='flex items-center gap-1'>
+        <Image
+          src = "/icons/logo.svg"
+          width = {32}
+          height = {32}
+          alt = "Logo"
+          className='max-sm:size-10'
+        />
+        <p className='text-[26px] font-extrabold text-white max-sm:hidden'>VidConnect</p>
+      </Link>
+      
+      <div className='ml-auto'>
+          <UserButton/>
+      </div>
+
+    <div className='flex-between gap-5'>
+      <MobileNav/> 
+    </div>
+
+    </nav>
+  )
+}
+
+export default Navbar
